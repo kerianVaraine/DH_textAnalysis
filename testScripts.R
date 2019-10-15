7486ecd2e17119d6df2b3d8201a37747b729d4cf
kerianVaraine <chambervulgarus@gmail.com>
2019-10-15 18:31:42 +1300

    Update testScripts.R
    TIDYTEXTDATA

--- testScripts.R
+++ testScripts.R
@@ -5,12 +5,19 @@ library(tidyverse)
 library(tidytext) 
 library(dplyr)
 library(stringr)
+library(stopwords)
 # library(tm)
 # library(textclean)
 
 #import original text from conversion, with line numbers, and chapter, skipping empty rows due to encoding
-text.original <- read_lines("src/Disappearance - Michael Joyce.txt", skip_empty_rows = TRUE, )
+text.original <- as_tibble(read_lines("src/Disappearance - Michael Joyce.txt", skip_empty_rows = TRUE, ))
 
   #View(text.original)
 
 #extract all chapter markings and apply them to the lines until the next chapter marking, and have these as a column
+text.chapters <- text.original %>% mutate(linenumber = row_number(), 
+                                          chapter = cumsum(str_detect(as.matrix(text.original), regex("[\\d]", ignore_case = TRUE))))
+
+                                          
+#Lay each word out, with line number and chapter.
+text.tidyish <- unnest_tokens(text.chapters, word, value)
